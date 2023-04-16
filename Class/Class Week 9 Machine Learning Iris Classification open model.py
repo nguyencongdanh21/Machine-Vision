dc943e349cd3ccd41473e743c6bead2c6bafda55
blowmeaway1234 <125113914+blowmeaway1234@users.noreply.github.com>
2023-04-16 12:07:21 +0700

    Delete Class directory

--- Class/Class Week 9 Machine Learning Iris Classification open model.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from sklearn.ensemble import AdaBoostClassifier
-import numpy as np
-
-#Open model trained
-model = pickle.load(open('model/Iris_classifier.sav','rb'))
-# data là  gia trị số liệu mới 
-y_pred = model.predict(data)
